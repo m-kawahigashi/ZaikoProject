cbb9f497881e940a54df027a1f1f8d70b9a389bd
m-kawhigashi <masanori0624ds16@yahoo.co.jp>
2020-05-02 15:37:43 +0900

    first commit

---
+++ app/src/main/java/com/example/zaikoproject/RegisterParts.java
@@ -0,0 +1,14 @@
+package com.example.zaikoproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterParts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_parts);
+    }
+}
---
+++ app/src/main/java/com/example/zaikoproject/RegisterUsers.java
@@ -0,0 +1,14 @@
+package com.example.zaikoproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_users);
+    }
+}
